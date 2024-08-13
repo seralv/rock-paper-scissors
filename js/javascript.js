6a95854f06a8be2ccd5ca28b8cdf01f19a4a9e08
seralv <sergioalvaro.14@gmail.com>
2024-08-13 17:50:57 -0400

    Update

--- js/javascript.js
+++ js/javascript.js
@@ -4,4 +4,21 @@ function getComputerChoice() {
   return signal[choice];
 }
 
-console.log("The computer choice is: " + getComputerChoice());
+function getHumanChoice() {
+  // return prompt("Choice rock, scissors or paper: ");
+}
+
+function playRound(humanChoice, computerChoice) {
+  let newHumanChoice = humanChoice.toLowerCase();
+  if (newHumanChoice == computerChoice) {
+    console.log(`!Empates¡ ${newHumanChoice} es igual a ${computerChoice}`);
+  } else if (newHumanChoice == "rock" && computerChoice == "paper") {
+    console.log(``);
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(`The computer choice is: ${getComputerChoice()}`);
+console.log(`The human choice: ${getHumanChoice()}`);
