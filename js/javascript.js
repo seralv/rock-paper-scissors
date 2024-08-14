e82b45c85f8d677733e942adf843849fa65f93d7
seralv <sergioalvaro.14@gmail.com>
2024-08-13 21:28:54 -0400

    Searching mistakes

--- js/javascript.js
+++ js/javascript.js
@@ -5,20 +5,48 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  // return prompt("Choice rock, scissors or paper: ");
+  return prompt("Choice rock, scissors or paper: ");
 }
 
 function playRound(humanChoice, computerChoice) {
   let newHumanChoice = humanChoice.toLowerCase();
+  console.log(`Your choice is: ${newHumanChoice}`);
+  console.log(`Computer choice is: ${computerChoice}`);
   if (newHumanChoice == computerChoice) {
-    console.log(`!Empates¡ ${newHumanChoice} es igual a ${computerChoice}`);
-  } else if (newHumanChoice == "rock" && computerChoice == "paper") {
-    console.log(``);
+    console.log(`¡Tied! ${newHumanChoice} is equal to ${computerChoice}`);
+    return "Tied";
+  } else if (
+    (newHumanChoice == "rock" && computerChoice == "scissors") ||
+    (newHumanChoice == "scissors" && computerChoice == "paper") ||
+    (newHumanChoice == "paper" && computerChoice == "rock")
+  ) {
+    console.log(`¡You win! ${newHumanChoice} wins to ${computerChoice}`);
+    return "Human wins";
+  } else {
+    console.log(`¡You lost! ${computerChoice} wins to ${newHumanChoice}`);
+    return "Computer wins";
   }
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(`The computer choice is: ${getComputerChoice()}`);
-console.log(`The human choice: ${getHumanChoice()}`);
+let result = playRound(getHumanChoice(), getComputerChoice());
+
+if (result == "Human wins") {
+  humanScore++;
+  console.log(`Human score: ${humanScore}`);
+  console.log(`Computer score: ${computerScore}`);
+  result = playRound(getHumanChoice(), getComputerChoice());
+} else if (result == "Computer wins") {
+  computerScore++;
+  console.log(`Human score: ${humanScore}`);
+  console.log(`Computer score: ${computerScore}`);
+  result = playRound(getHumanChoice(), getComputerChoice());
+} else {
+  if (humanScore > computerScore) {
+    console.log(`¡You win with ${humanScore} points!`);
+  } else {
+    console.log(`¡Computer wins with ${computerScore} points!`);
+  }
+}
